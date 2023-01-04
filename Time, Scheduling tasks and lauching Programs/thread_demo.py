35d968c5ac022e8016a17c7a9a3510e5c8f15c37
ClaireRuysschaert <netclaire@hotmail.fr>
2023-01-04 19:19:26 +0100

    Multi threading

---
+++ Time, Scheduling tasks and lauching Programs/thread_demo.py
@@ -0,0 +1,17 @@
+import threading, time
+print('Start of program.')
+
+def take_a_nap():
+    time.sleep(5)
+    print('Wake up!')
+
+thread_obj = threading.Thread(target=take_a_nap)
+thread_obj.start()
+
+print('End of program.')
+
+# Passing arguments in args with a list and keyword arg in a dict
+thread_obj_2 = threading.Thread(target=print, args=['Yuki', 'TDB'], kwargs={'sep' : ' & '})
+thread_obj_2.start()
+
+# avoir concurrency issues : never let multiple threads read/write the same variables
